89017eb24be00c9b40b027d9996103cece86054c
“lilizhi <@lilizhi>
2024-09-23 13:48:59 +0800

    cloud配置

--- llz_springboot-start/src/main/java/com/llz/LlzDemo2SpringbootApplication.java
+++ llz_springboot-start/src/main/java/com/llz/LlzDemo2SpringbootApplication.java
@@ -3,13 +3,15 @@ package com.llz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.llz.dao")
-public class LlzSpringbootApplication {
+@EnableEurekaClient
+public class LlzDemo2SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LlzSpringbootApplication.class, args);
+        SpringApplication.run(LlzDemo2SpringbootApplication.class, args);
     }
 
 }
